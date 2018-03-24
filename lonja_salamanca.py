9f9427304b10cf3918d7c922892f072106e41a03
emartigo <37473236+emartigo@users.noreply.github.com>
2018-03-24 13:14:53 +0100

    Update lonja_salamanca.py

--- lonja_salamanca.py
+++ lonja_salamanca.py
@@ -1,4 +1,3 @@
-#!/usr/bin/python
 # coding=utf-8
 import sys
 import os
@@ -10,32 +9,36 @@ from datetime import timedelta
 from bs4 import BeautifulSoup
 #Captura de los comandos de fecha de inicio y de fin
 parser = argparse.ArgumentParser()
-parser.add_argument("--startDate", help="Enter start date of interval")
-parser.add_argument("--endDate", help="Enter end date of interval")
+parser.add_argument("--startDate", help="Fecha de inicio del intervalo")
+parser.add_argument("--endDate", help="Fecha de fin del intervalo")
 args = parser.parse_args()
 reload(sys)
-sys.setdefaultencoding('utf8')
+def to_unicode_or_bust(obj, encoding="latin1"):
+    if isinstance(obj, basestring):
+        if not isinstance(obj, unicode):
+            obj=unicode(obj, encoding)
+    return obj
 #Función para obtener los datos
 def queryPrices( queryURL, paramsValues,headersValues,elementList,date2Filter ):
     paramsValues['fecha']=date2Filter
     response= requests.post(queryURL, data = paramsValues, headers=headersValues)
     soup = BeautifulSoup(response.text,"html.parser")
-    table=soup.find('table',{'border':'1'});//Identificación de la tabla que contiene los datos a almacenar
+    table=soup.find('table',{'border':'1'});#Identificación de la tabla que contiene los datos a almacenar
     currentIndex=0
     for row in table.findAll("tr"):
         cells = row.findAll('td')
         if (currentIndex > 0):
             if len(cells)==8:
-                Producto=cells[0].find(text=True)
-                Categoria=cells[1].find(text=True)
+                Producto=cells[0].find(text=True).encode("latin1")
+                Categoria=cells[1].find(text=True).encode("latin1")
                 Anterior=cells[2].find(text=True)
                 Ultimo=cells[3].find(text=True)
                 Dif=cells[4].find(text=True)
                 Media=cells[5].find(text=True)
                 FechaTabla=cells[7].find(text=True)
             else:
-                Producto=cells[0].find(text=True)
-                Categoria=cells[1].find(text=True)
+                Producto=cells[0].find(text=True).encode("latin1")
+                Categoria=cells[1].find(text=True).encode("latin1")
                 Anterior=cells[2].find(text=True)
                 Ultimo=cells[3].find(text=True)
                 Dif=cells[4].find(text=True)
@@ -78,11 +81,11 @@ headerList=["Producto","Categoria","Anterior","Ultimo","Dif","Medida","Fecha ","
 priceList.append(headerList)
 while startDate <= endDate:
     currentDate = startDate.strftime('%d/%m/%Y')
-    print ("Generating dataset of %s" %  currentDate)
+    print ("Generando base de datos %s" %  currentDate)
     queryPrices(url,formData,headerValues,priceList,currentDate)
     startDate = startDate + timedelta(days=7) #Realizamos una consulta semanal desde la fecha de inicio hasta la fecha de fin
 #Escritura del csv  
-with open(filePath, 'w') as csvFile:
+with open(filePath, 'w')as csvFile:
     writer = csv.writer(csvFile)
     for priceElement in priceList:
         writer.writerow(priceElement)
